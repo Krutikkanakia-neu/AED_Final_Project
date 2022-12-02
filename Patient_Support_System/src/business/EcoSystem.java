3abf2b8454962cce526611889f5e64e287294931
krutik kanakia <kanakia.k@northeastern.edu>
2022-12-02 15:20:14 -0500

    Business package

--- Patient_Support_System/src/business/EcoSystem.java
+++ Patient_Support_System/src/business/EcoSystem.java
@@ -5,10 +5,84 @@
  */
 package business;
 
-/**
- *
- * @author Admin
- */
-public class EcoSystem {
+import business.commonUtils.NeedHelp;
+import business.networkpkg.Network;
+import business.organizationpkg.Organization;
+import business.rolepkg.Role;
+import business.rolepkg.SystemAdminRole;
+import java.util.ArrayList;
+
+
+public class EcoSystem extends Organization{
+ 
+   private ArrayList<Network> networkList;
+   private static EcoSystem ecoSystem;
+   private ArrayList<NeedHelp> helpList;
+    
+   public EcoSystem()
+   {
+       super(null);
+       networkList = new ArrayList<>();
+       helpList = new ArrayList<>();
+      
+   }
+   
+   public static EcoSystem getInstance()
+   {
+    if(ecoSystem == null)
+    {
+      ecoSystem = new EcoSystem();
+    }
+    return ecoSystem;
+   }
+
+    public ArrayList<Network> getNetworkList() {
+        return networkList;
+    }
+   
+    public Network addNetwork()
+    {
+        Network network = new Network();
+        networkList.add(network);
+        return network;
+    }
+    
+    public boolean checkIfUsernameIsUnique(String username)
+    {
+      if (!getUserAccountDirectory().checkIfUsernameIsUnique(username)) {
+        return false;
+      }
+      return true;
+    }
+   
+    public void deleteNetwork(Network network)
+    {
+        networkList.remove(network);
+    }
+   
+   @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roleList = new ArrayList<>();
+        roleList.add(new SystemAdminRole());
+        return roleList;
+    }
+   
+    public NeedHelp addNeedHelp()
+    {
+        NeedHelp needHelp = new NeedHelp();
+        helpList.add(needHelp);
+        return needHelp;
+    }
+    
+    public void deleteHelp(NeedHelp needHelp)
+    {
+        helpList.remove(needHelp);
+    }
+
+    public ArrayList<NeedHelp> getHelpList() {
+        return helpList;
+    }
+    
+    
     
 }
